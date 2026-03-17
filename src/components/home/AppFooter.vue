dede2f4e22fd7377bed89fe1b5ec7aa1762407ec
morkmorquecho <mork.morquecho@gmail.com>
2026-03-17 07:59:54 -0600

    fix footer

--- src/components/home/AppFooter.vue
+++ src/components/home/AppFooter.vue
@@ -36,7 +36,12 @@
               <rect x="2" y="4" width="20" height="16" rx="2"/>
               <path d="M2 7l10 7 10-7"/>
             </svg>
-            <a href="mailto:lorenza.arte.enpapel@gmail.com" class="footer__link">
+            <a 
+              href="https://mail.google.com/mail/?view=cm&to=lorenza.arte.enpapel@gmail.com" 
+              target="_blank"
+              rel="noopener noreferrer"
+              class="footer__link"
+            >
               lorenza.arte.enpapel@gmail.com
             </a>
           </li>
@@ -45,7 +50,7 @@
               <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/>
             </svg>
             <a :href="whatsappUrl" target="_blank" rel="noopener" class="footer__link">
-              +52 314 104 2758
+              +52 314 101 0990
             </a>
           </li>
         </ul>
@@ -103,7 +108,7 @@ export default defineComponent({
     ]
 
     const whatsappUrl = computed(() =>
-      `https://wa.me/523141042758?text=${encodeURIComponent('Hola, me interesa conocer más sobre Colección Lorenza.')}`
+      `https://wa.me/523141010990?text=${encodeURIComponent('Hola, me interesa conocer más sobre Colección Lorenza.')}`
     )
 
     const socials = [
