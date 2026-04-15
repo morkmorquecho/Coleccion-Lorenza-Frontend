fe440938767329d98a495636f624120a4f676c05
morkmorquecho <mork.morquecho@gmail.com>
2026-04-15 09:10:08 -0600

    global config

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import router from './router'
 import './assets/main.css'
 import AOS from "aos"
 import "aos/dist/aos.css"
+import '@/assets/forms.css'
+import { setupInterceptors } from '@/services/api' 
 
 const app = createApp(App)
+const pinia = createPinia()
 
 AOS.init()
-app.use(createPinia()) 
+app.use(pinia) 
 app.use(router)         
+setupInterceptors(pinia) 
 
 app.mount('#app')
